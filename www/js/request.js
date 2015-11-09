c2f3b3b6668a02b8c5bd5c7f9c5be7808861302e
slavunya_ua <slavunya.ua@gmail.com>
2015-11-09 18:05:35 +0200

    keyboard

--- www/js/request.js
+++ www/js/request.js
@@ -32,15 +32,15 @@ $(document).ready(function () {
 
 function keyboardShowHandler() {
     if (menu === 1) {
-        $('.parentControlBottomButtons').height('0');
+        $('.parentControlBottomButtons').hide();
     }
     window.scrollTo(0, 100);
 }
 function keyboardHideHandler() {
     if (menu === 1) {
         setTimeout(function () {
-            $('.parentControlBottomButtons').height('61px');
-        }, 1500);
+            $('.parentControlBottomButtons').show();
+        }, 1000);
     }
 }
 
